733b01fdf9764f6193ddc9355b4884c4f24be50c
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-01-15 11:28:34 -0500

    homepage updated to only filter and display current day's events

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,20 +33,14 @@ router.get('/event/:id', async (req, res) => {
 
 router.get('/profile', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
+    
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Event }],
     });
-    console.log(userData);
-    const events = await Event.findAll(
-      {
-        include: [User]
-      }
-    )
-    // const dishes = dishData.map((dish) => dish.get({ plain: true }));
-    console.log(events);
-    // const user = userData.get({ plain: true });
+
+    const user = userData.get({ plain: true });
+
     res.render('profile', {
       ...user,
       logged_in: true
@@ -95,70 +89,75 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get the current date in the format YYYY-MM-DD
-//     const currentDate = new Date().toISOString().split('T')[0];
-//     console.log(currentDate);
-
-//     // Get all events for the current day and JOIN with user data
-//     const eventData = await Event.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//       where: {
-//         date: {
-//           [Op.eq]: `${currentDate}`, // filter events for current day
-//         },
-//       },
-//     });
-
-//     // Serialize data so the template can read it
-//     const events = eventData.map((event) => event.get({ plain: true }));
-//     // Pass serialized data and session flag into the template
-//     res.render('homepage', {
-//       events,
-//       logged_in: req.session.logged_in
-
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/', async (req, res) => {
   try {
-    // Get all events and JOIN with user data
+    // Get the current date in the format YYYY-MM-DD
+    const currentDate = new Date().toISOString().split('T')[0];
+    console.log(currentDate);
 
+    // Get all events for the current day and JOIN with user data
     const eventData = await Event.findAll({
       include: [
         {
           model: User,
           attributes: ['name'],
         },
-        {
-          model: Image,
-          arribute: ['url'],
-        }
+            {
+              model: Image,
+              arributes: ['url'],
+            }
       ],
+      where: {
+        date: {
+          [Op.eq]: `${currentDate}`, // filter events for current day
+        },
+      },
     });
 
-
-    // Serialize data so the template can read it
+    
     const events = eventData.map((event) => event.get({ plain: true }));
-    // Pass serialized data and session flag into template
-    console.log(events);
+   
     res.render('homepage', {
       events,
       logged_in: req.session.logged_in
+
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all events and JOIN with user data
+
+//     const eventData = await Event.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//         {
+//           model: Image,
+//           arributes: ['url'],
+//         }
+//       ],
+//     });
+
+
+//     // Serialize data so the template can read it
+//     const events = eventData.map((event) => event.get({ plain: true }));
+//     // Pass serialized data and session flag into template
+//     console.log(events);
+//     res.render('homepage', {
+//       events,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
 
